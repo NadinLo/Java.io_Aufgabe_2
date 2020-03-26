d18096ec7f8a7c7da4cc326e84e1e5c51df89509
NadinLo <nadin.loewe@posteo.de>
2020-03-26 16:47:02 +0100

    integrate  organization chart

--- src/com/company/Department.java
+++ src/com/company/Department.java
@@ -4,13 +4,48 @@ public class Department {
     String name;
     Person[] staff = new Person[100];
     int counterStaff = 0;
+    int amountSubDeps;
+    int counterSubDeps;
+    Department[] subDeps;
+    Department subDep;
+    Department parentDep;
 
     public Department (String name){
         this.name = name;
+        //this.amountSubDeps = amountSubDeps;
+        this.subDeps = new Department[5];             //zunächst immer 5
     }
 
     public void employed (Person person){
         staff[counterStaff] = person;
         counterStaff++;
+    }
+
+    public void createSubDep (Department department){
+        this.subDep = department;
+        this.subDeps[this.counterSubDeps] = this.subDep;
+        this.counterSubDeps++;
+    }
+
+    public void createParentDep (Department department){
+        this.parentDep = department;
+    }
+
+    public  void printSubDep(String tab) {
+        for (int i = 0; i < this.subDeps.length; i++) {
+            if (this.subDeps[i] != null) {
+                System.out.print(tab + this.subDeps[i].name + " (");
+                for (int j = 0; j < this.subDeps[i].staff.length; j++) {
+                    if (this.subDeps[i].staff[j] != null){
+                        System.out.print(this.subDeps[i].staff[j].name);
+                    }
+                    System.out.println(")");
+                    break;
+
+                }
+                tab = "\t\t" + tab;
+                this.subDeps[i].printSubDep(tab);
+            }
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        File myFile = new File("C:\\Users\\DCV\\Documents\\Coding\\Java IO\\Texte\\Abteilungen.txt");
+        File myFile = new File("C:\\Users\\DCV\\Documents\\Coding\\Java IO\\Texte\\Abteilungen1.txt");
         FileReader fileReader = new FileReader(myFile);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
@@ -14,55 +14,106 @@ public class Main {
         Department[] departments = new Department[100];
         int counterDepartments = 0;
 
-        String line;
-
         //skip first line
-        bufferedReader.skip(bufferedReader.readLine().length() + 1);
-        //read all lines after till null
+        bufferedReader.readLine();
+        String line = null;
+
+        //read all lines after first line till null
         while ((line = bufferedReader.readLine()) != null) {
             String[] splittedValues = line.split(";");
             String name = splittedValues[0].strip();
             String nameDepartment = splittedValues[1].strip();
+
             //check if Person already exists:
             Person person = null;
-            for (Person employee : employees) {
-                if (employee != null) {
-                    //if so go on with this existing Person
-                    if (employee.name.equals(name)) {
-                        person = employee;
-                        break;
-                    }
-                    //if not, add a new one to the array
-                } else {
+            for (int i = 0; i < employees.length; i++) {
+                //if not create new Person
+                if (employees[i] == null) {
                     person = new Person(name);
-                    employees[counterEmployees] = person;
+                    employees[i] = person;
                     counterEmployees++;
                     break;
+                    //if so, go on with this person
                 }
+                if (employees[i].name.equals(name)) {
+                    person = employees[i];
+                    break;
+                }
+
             }
             //check if Department already exists:
             Department department = null;
-            for (Department department1 : departments) {
-                if (department1 != null) {
-                    //if so go on with this existing department
-                    if (department1.name.equals(nameDepartment)) {
-                        department = department1;
-                        break;
-                    }
-                    //if not add a new one to the array
-                } else {
+            for (int i = 0; i < departments.length; i++) {
+                //if not create a new one
+                if (departments[i] == null) {
                     department = new Department(nameDepartment);
-                    departments[counterDepartments] = department;
+                    departments[i] = department;
                     counterDepartments++;
                     break;
+                    //if so, go on with this department
+                }
+                if (departments[i].name.equals(nameDepartment)) {
+                    department = departments[i];
+                    break;
+                }
+
+            }
+
+            //connect both:
+            person.worksAt(department);
+            department.employed(person);
+
+            if (splittedValues.length == 3) {
+                String nameParentDepartment = splittedValues[2].strip();
+                //check if ParentDepartment already exists:
+                Department parentDep = null;
+                for (int i = 0; i < departments.length; i++) {
+                    //if not create a new one
+                    if (departments[i] == null) {
+                        parentDep = new Department(nameParentDepartment);
+                        departments[i] = parentDep;
+                        counterDepartments++;
+                        break;
+                        //if so, go on with this department
+                    }
+                    if (departments[i].name.equals(nameParentDepartment)) {
+                        parentDep = departments[i];
+                        break;
+                    }
+                }
+                //connecting ParentDepartment with SubDepartment:
+                //is connection already given?
+                for (int i = 0; i < parentDep.subDeps.length; i++) {
+                    if (parentDep.subDeps[i] != null) {
+                        if (parentDep.subDeps[i].name.equals(department.name)) {
+                            break;
+                        }
+                    } else {
+                        parentDep.createSubDep(department);
+                        department.createParentDep(parentDep);
+                        break;
+                    }
                 }
             }
-            //connect both:
-            assert person != null;
-            person.worksAt(department);
-            assert department != null;
-            department.employed(person);
         }
-        System.out.println("ready");
+
+        String printDepartment = "";
+        //check which department has no parentDepartment:
+        for (Department department : departments) {
+            if (department.parentDep == null) {
+                printDepartment = printDepartment + department.name + ": (";
+                String printStaff = "";
+                for (int j = 0; j < department.staff.length; j++) {
+                    if (department.staff[j] != null) {
+                        printStaff = printStaff + department.staff[j].name + " ";
+                    }
+                }
+                System.out.println(printDepartment + printStaff + ")");
+                department.printSubDep("\t|_");
+                break;
+
+            }
+        }
     }
 }
+
